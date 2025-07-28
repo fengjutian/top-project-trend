89dcf5e2c119ab7913536f828f09874b0b55d39e
fengjutian <fjutian@foxmail.com>
2025-07-28 14:36:44 +0800

    feat: nuxt 主题

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -151,7 +151,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} fengjutian. Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
